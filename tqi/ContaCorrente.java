de9eca7605cb67a4072858d00a4894d9256e6522
LeonardoMeneghini <87610559+LeonardoMeneghini@users.noreply.github.com>
2022-07-13 01:11:59 -0300

    Modificação nas classes

---
+++ tqi/ContaCorrente.java
@@ -0,0 +1,9 @@
+package tqi;
+
+public class ContaCorrente extends Conta {
+    private static int SEQUENCIAL = 1;
+    public ContaCorrente(){
+        super.agencia = Conta.AGENCIA_PADRAO;
+        super.numero = SEQUENCIAL++;
+    }
+}
---
+++ tqi/IConta.java
@@ -0,0 +1,7 @@
+package tqi;
+
+public interface IConta {
+    void sacar(double valor);
+    void depositar(double valor);
+    void transferir(double valor, Conta contaDestino);
+}
